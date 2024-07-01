9c80a261fbe9f238473e8bd05051a1823ed3eff2
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:22:19 +0530

    Update main.cpp

--- C++ number-based-program/5. W.A.P to reverse the all digits in a given supplied integer number/main.cpp
+++ C++ number-based-program/5. W.A.P to reverse the all digits in a given supplied integer number/main.cpp
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 /* 5. W.A.P to reverse the all digits in a given supplied integer number.
 Input: int data=12345
 output: data=54321. */
