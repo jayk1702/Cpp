11ef5d32db52b9f4ab17024e989b90de5a7ee7b8
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:20:52 +0530

    Update main.cpp

--- C++ number-based-program/3. W.A.P to calculate the sum of all even digits in a given supplied  integer number/main.cpp
+++ C++ number-based-program/3. W.A.P to calculate the sum of all even digits in a given supplied  integer number/main.cpp
@@ -1,18 +1,8 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 /* 3. W.A.P to calculate the sum of all even digits in a given supplied 
 integer number.
 Input: int data=1234
 Output : sum of all digits is 6 
 here 2,4 are the even digit. */
-
 
 #include <iostream>
 using namespace std;
