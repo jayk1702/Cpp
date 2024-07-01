c498f3fda2f8fc8d7a2e65c4a6fc2bf25372075d
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:22:40 +0530

    Update main.cpp

--- C++ number-based-program/6.W.A.P to delete the given digit in a given integer/main.cpp
+++ C++ number-based-program/6.W.A.P to delete the given digit in a given integer/main.cpp
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 /* 6.W.A.P to delete the given digit in a given integer.
 e.g: Input int data=132345;
  digit=3;
