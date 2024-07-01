40f4e18638d8311221d758a31e23d33aa81ea2b8
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:17:19 +0530

    Update main.cpp

--- C++ number-based-program/1. W.A.P to count the digits in a given supplied integer number/main.cpp
+++ C++ number-based-program/1. W.A.P to count the digits in a given supplied integer number/main.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 /*1. W.A.P to count the digits in a given supplied integer number.
 e.g: if given integer is int data= 12345.
