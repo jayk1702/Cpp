bfc960d0ee297fe0fa247558cc1c279b3cf35ea9
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:24:53 +0530

    Update main.cpp

--- C++ number-based-program/9. W.A.P to check digits in given integer number either Ascending or Descending Order/main.cpp
+++ C++ number-based-program/9. W.A.P to check digits in given integer number either Ascending or Descending Order/main.cpp
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 /* 9. W.A.P to check digits in given integer number either Ascending or
 Descending Order.
 e.g: Input int data=1234.
