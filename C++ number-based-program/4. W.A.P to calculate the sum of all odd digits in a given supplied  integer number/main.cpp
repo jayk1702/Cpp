081eef16bedc52a191ea90687da985d88b832c0b
jayk1702 <165367555+jayk1702@users.noreply.github.com>
2024-07-01 12:21:27 +0530

    Update main.cpp

--- C++ number-based-program/4. W.A.P to calculate the sum of all odd digits in a given supplied  integer number/main.cpp
+++ C++ number-based-program/4. W.A.P to calculate the sum of all odd digits in a given supplied  integer number/main.cpp
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 /* 4. W.A.P to calculate the sum of all odd digits in a given supplied 
 integer number.
 Input: int data=1234
